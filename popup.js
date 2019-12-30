c2ba5640badd501408a0f9da8377b739e64afa70
johannagran <johanna.granstrom@gmail.com>
2019-12-30 23:17:54 +0100

    fixat bg osv

--- popup.js
+++ popup.js
@@ -1,4 +1,13 @@
+//function myFunction() {
+//    var popup = document.getElementById("myPopup");
+//    popup.classList.toggle("show");
+//}
+
 function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+    var x = document.getElementById("info");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
