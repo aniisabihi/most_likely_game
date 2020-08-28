302591fe9915a4c50b0513008e8bdcbf23a78fda
Aniisa Bihi <aniisaaden@gmail.com>
2020-08-28 18:22:25 +0200

    New version - English

--- popup.js
+++ popup.js
@@ -1,8 +1,3 @@
-//function myFunction() {
-//    var popup = document.getElementById("myPopup");
-//    popup.classList.toggle("show");
-//}
-
 function myFunction() {
     var x = document.getElementById("info");
     if (x.style.display === "none") {
